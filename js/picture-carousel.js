9f6131f7fd382bca8610fee928272c20ef783c98
Christopher-KT <ckesslertiffany@gmail.com>
2024-09-05 16:33:52 -0600

    added meta data and seperated css and js files for better organization

--- js/picture-carousel.js
+++ js/picture-carousel.js
@@ -1,32 +1,3 @@
-function openInNewTab() {
-    window.open("https://bg.battletech.com", "_blank").focus();
-  }
-
-document.getElementById('submitBtn').disabled = true;
-
-    function displayMessage() {
-        alert('The Cake is a Lie!');
-    }
-
-    function changeColor(element, color) {
-        element.style.backgroundColor = color;
-    }
-
-    document.addEventListener('DOMContentLoaded', function() {
-        const items = document.querySelectorAll('.carousel-item');
-        let currentIndex = 0;
-
-        function cycleItems() {
-            const item = items[currentIndex];
-            item.classList.remove('active');
-
-            currentIndex = (currentIndex + 1) % items.length;
-            items[currentIndex].classList.add('active');
-        }
-        setInterval(cycleItems, 3000);
-    }
-);
-
 // HW 10 Carousel
 const carouselSlide = document.querySelector('.carousel-slide');
 const carouselItems = document.querySelectorAll('.carousel-item');
